8956b4eecdac6b10c37ec8d6d9979170a71248b3
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-05 16:54:13 -0300

    .

--- condicionais/ex04/script.js
+++ condicionais/ex04/script.js
@@ -1,6 +1,15 @@
 function verificar() {
-    var txt1 = parseFloat(document.getElementById("numero1").value);
-    var txt2 = parseFloat(document.getElementById("numero2").value);
-    var txi1 = txt1;
-    
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var numero3 = parseFloat(document.getElementById("numero3").value);
+    var resposta = document.querySelector("p");
+    if (numero1 > numero2 && numero1 > numero3) {
+        resposta.innerHTML = `o numero ${numero1} e maior que ${numero2} e ${numero3}`;
+    } else if (numero2 > numero1 && numero2 > numero3) {
+        resposta.innerHTML = `o numero ${numero2} e maior que ${numero1} e ${numero3}`;
+    } else if (numero3 > numero1 && numero3 > numero2) {
+        resposta.innerHTML = `o numero ${numero3} e maior que ${numero1} e ${numero2}`;
+    } else {
+        resposta.innerHTML = "[ERRO} verifique os dados e tente novamente";
+    }
 }
---
+++ condicionais/ex05/script.js
@@ -0,0 +1,6 @@
+function verificar() {
+    var txt1 = parseFloat(document.getElementById("numero1").value);
+    var txt2 = parseFloat(document.getElementById("numero2").value);
+    var txi1 = txt1;
+    
+}
